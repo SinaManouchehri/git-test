65182e193852e945291dc028fbbad110c7165f35
Sina Manouchehri <Sina.manouchehri7@gmail.com>
2025-10-13 14:03:35 +0330

    greeting with user

--- work.py
+++ work.py
@@ -2,3 +2,6 @@ print("This is the start of global work👊💪")
 print("I'm proud of myself.")
 print("This is the power of God 💪💪💪")
 print("Thanks my dear God.")
+a="Sina"
+print(f'''Hello {a},/
+      Welcome to the global.''')
